bfd6d3b0a3f728aed5e9b6af2fd0e4e518b599a2
Emilio Pizarro <emiliopizarro866@gmail.com>
2026-01-16 01:05:31 -0300

    Autenticacion de Usuarios

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,21 +1,40 @@
 import mongoose, { Schema } from 'mongoose'
 
+//El interface es un espejo del Schema.
+//Informacion que requiere Typescript
+export interface IUser{
+    handle:string
+    name:string
+    email:string
+    password:string
 
+}
+
+
+ 
 //definimos el schema , forma de los datos
 //en mongo ,se define el schema y luego se asocia al modelo
 
 const userSchema = new Schema({
  
+    handle:{
+        type:String,
+        require: true,
+        trim:true,
+        lowercase:true,
+        unique:true
+    }, 
     name:{
         type:String,
         require: true,
         trim:true
-    },
+    }, 
     email:{
         type:String,
         require: true,
         trim:true,
-        unique:true
+        unique:true,
+        lowercase:true
     },
     password:{
         type:String,
@@ -27,6 +46,7 @@ const userSchema = new Schema({
 })
 
 //defino el modelo, con el nombre que lo defino y el esquema que usa
-const User = mongoose.model('User',userSchema)
+//<User> es un Generic asociado a User 
+const User  = mongoose.model<IUser>('User',userSchema)
 //Lo exporto para poder usarlo en cualquier parte del codigo
 export default User;
